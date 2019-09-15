e4c17ecd31205c98134818e70509a3870cc5d29c
Youngdae Heo <ck4ckei@gmail.com>
2019-09-15 21:59:12 +0900

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,13 @@
+# tweepy keys & tokens
+consumer_key = ''
+consumer_secret = ''
+
+access_token = ''
+access_token_secret = ''
+
+# MySQL connection info
+mysql_host = ''
+mysql_port = 3306
+mysql_user = ''
+mysql_password = ''
+mysql_database = ''
